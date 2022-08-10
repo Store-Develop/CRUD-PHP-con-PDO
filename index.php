a20b83c73193dffbb08a037fe62da423c961ff90
Juan Perez <jkml563@gmail.com>
2022-08-10 12:32:06 -0500

    Menejo y Comparación de string en php

--- index.php
+++ index.php
@@ -5,24 +5,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+
+        .resaltar{
+            color: red;
+            font-weight: bold;
+        }
+
+    </style>
 </head>
 <body>
     <?php
-        function incrementaVariable(){
-            static $contador = 0;// variable estatica, se ejecuta solo la primera vez que se llama la función y el valor se conserva al finalizar la función.
+    $nombre = "Juan";
 
-            $contador += 1;
+        echo "nombre $nombre";
+        echo "<p class='resaltar'>Esto es un ejemplo de frase</p>";
+        //echo '<p class="resaltar">Esto es un ejemplo de frase</p>'
+        // echo "<p class=\'resaltar\'>Esto es un ejemplo de frase</p>" // escapar string
 
-            echo $contador . "<br>";
+        //Comparación de string 
+
+        $variable1="casa";
+        $variable2="CASA";
+        $resultado=strcmp($variable1, $variable2); // devuelve 1 si no son iguales y 0 sin iguales.
+        //strcmp($variable1, $variable2); compara teniendo en cuenta las mayuscalas y minusculas.
+        //echo "El resultado es: $resultado";
+        $resultado2=strcasecmp($variable1, $variable2); // devuelve 1 si no son iguales y 0 sin iguales.
+        //strcasecmp($variable1, $variable2); compara sin tener en cuenta las mayuscalas y minusculas.
+        //echo "<br>Con strcasecmp el resultado es: $resultado2";
+
+        if($resultado === 0){
+            echo "La variable #1 con el valor de $variable1 y la variable #2 con el valor de $variable2 ¡SON IGUALES<br>";
+        }else {
+            echo "La variable #1 con el valor de $variable1 y la variable #2 con el valor de $variable2 ¡NO SON IGUALES<br>";
         }
 
-        incrementaVariable();
-        incrementaVariable();
+        if ($resultado2 === 0) {
+            echo "La variable #1 con el valor de $variable1 y la variable #2 con el valor de $variable2 ¡SIGNIFICAN LO MISMO Y NO ESTAN ESCRITAS IGUAL.<br>";
+        }else {
+            echo "La variable #1 con el valor de $variable1 y la variable #2 con el valor de $variable2 ¡NO SON IGUALES";
+        }
 
-        incrementaVariable();
-        incrementaVariable();
-        incrementaVariable();
-        
+
     ?>
 </body>
 </html>
