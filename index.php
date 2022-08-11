f67ee085f7216af816bc4bc0b4909d0833ae599d
Juan Perez <jkml563@gmail.com>
2022-08-11 16:05:42 -0500

    Operadores matemáticos 2

--- index.php
+++ index.php
@@ -1,61 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilos.css" type="text/css">
-    <title>Document</title>
-    
-</head>
-<body>
-    <form nema="form1" method="POST" action="">
-        <p>
-        <label for="num1"></label>
-        <input type="number" name="num1" id="num1"/>
-        <label for="num2"></label>
-        <input type="number" name="num2" id="num2"/>
-        <label for="operacion"></label>
-        <select name="operacion" id="operacion">
-            <option>Suma</option>
-            <option>Resta</option>
-            <option>Multiplicación</option>
-            <option>Division</option>
-            <option>Modulo</option>
-        </select>
-        </p>
-        <p>
-            <input type="submit" name="button" id="button" value="Enviar" onclick="prueba"/>
-        </p>
-    </form>
-    <p>&nbsp;</p>
+<style>
+    .resultado{
+        color: red;
+        font-weight: bold;
+        font-size: 32px;
 
-    <?php
+    }
+</style>
 
-        if (isset($_POST["button"])) {
-            
-            $numero1 = $_POST["num1"];
-            $numero2 = $_POST["num2"];
-            $operacion = $_POST["operacion"];
+<?php
 
-            if (!strcmp("Suma", $operacion)) {
-                
-                echo "El resultado es: " . ($numero1 + $numero2);
-            }
-            elseif(!strcmp("Resta", $operacion))
-            {
-                echo "El resultado es: " . ($numero1 - $numero2);
-            }
-            elseif(!strcmp("Multiplicación", $operacion))
-            {
-                echo "El resultado es: " . ($numero1 * $numero2);
-            }
-            elseif(!strcmp("Division", $operacion))
-            {
-                echo "El resultado es: " . ($numero1 / $numero2);
-            }
-            elseif(!strcmp("Modulo", $operacion))
-            {
-                echo "El resultado es: " . ($numero1 % $numero2);
-            }
-        }
+if (isset($_POST["button"])) {
+
+    $numero1 = $_POST["num1"];
+    $numero2 = $_POST["num2"];
+    $operacion = $_POST["operacion"];
+    Calcular($operacion);
+}
+
+function Calcular($calculo){
+    if (!strcmp("Suma", $calculo)) 
+    {
+        global $numero1;
+        global $numero2;
+
+        $resultado = ($numero1 + $numero2);
+        echo "<p class='resultado'>El resultado es: $resultado </p>";
+        
+    } elseif (!strcmp("Resta", $calculo)) {
+        global $numero1;
+        global $numero2;
+
+        $resultado = ($numero1 - $numero2);
+        echo "<p class='resultado'>El resultado es: $resultado </p>";
+    } elseif (!strcmp("Multiplicación", $calculo)) {
+        global $numero1;
+        global $numero2;
+
+        $resultado = ($numero1 * $numero2);
+        echo "<p class='resultado'>El resultado es: $resultado </p>";
+    } elseif (!strcmp("Division", $calculo)) {
+        global $numero1;
+        global $numero2;
+
+        $resultado = ($numero1 / $numero2);
+        echo "<p class='resultado'>El resultado es: $resultado </p>";
+    } elseif (!strcmp("Modulo", $calculo)) {
+        global $numero1;
+        global $numero2;
+
+        $resultado = ($numero1 % $numero2);
+        echo "<p class='resultado'>El resultado es: $resultado </p>";
+    }
+
+}
