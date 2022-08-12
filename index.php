b2527a4459a50478fbbaa10f372dc3900086d9d8
Juan Perez <jkml563@gmail.com>
2022-08-12 11:51:58 -0500

    Condicionales 3. Switch case

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
 <?php
 
-//Operador ternario ?.
+//Condicionales switch case.
 
-    if(isset($_POST["enviado"])){
-        $n_usuario = $_POST["n_usuario"];
-        $contraseña = $_POST["contra"];
+if (isset($_POST["enviado"])) {
+    $n_usuario = $_POST["n_usuario"];
+    $contraseña = $_POST["contra"];
+    $perfil = $_POST["perfil"];
 
-        /*if ($edad < 18) {
-            echo "Eres Menos de edad. No tienes acceso";
-        }else {
-            echo "Eres mayor de edad. Puedes acceder";
-        }*/
 
-        $acceso = $n_usuario=="Juan" && $contraseña == "1234" ? "¡¡¡Bienvenido!!!" : "Nombre de usuario o Contraseña incorrectos Por favor validar";
+    switch ($perfil) {
+        case 'SAD' && $n_usuario == "Juan" && $contraseña == "J1c2":
+                echo "¡¡¡Bienvenido(a) Super Administrador del sistema!";
+            break;
+        case 'AD' && $n_usuario == "Sebastian" && $contraseña == "Aux1":
+            
+                echo "¡¡¡Bienvenido(a) Administrador del sistema!";
+            break;
+        case 'EN' && $n_usuario == "Alejandra" && $contraseña == "AprI":
 
-        echo $acceso;
+                echo "¡¡¡Bienvenido(a) Encargado del sistema!.";
+            break;
+        case 'E' && $n_usuario == "Mari" && $contraseña == "Sst1":
+       
+                echo "¡Bienvenido(a) Empleado de la compañia!";          
+            break;
+        case 'V' && $n_usuario == "VstEmp" && $contraseña == "Visitante#":
 
+                echo "¡Bienvenido(a) ha nuestro sistema sacale provecho!"; 
+            break;
+        default:
+        echo "Nombre de usuario o Contraseña incorrectos por favor validar.";
     }
-
-
-
-
-
-?>
+}
