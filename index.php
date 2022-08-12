664a3d65b4fd25cc7dad0c136332a08817771ac7
Juan Perez <jkml563@gmail.com>
2022-08-12 16:26:41 -0500

    POO creación he instanciación de Clases en PHP.

--- index.php
+++ index.php
@@ -1,39 +1,18 @@
 <?php
-
-//Funciones en PHP. 
-
-//Funciones con paso de parametros por valor y paso por referencia para paso por valor se pasa la variable normal
-//Ejemplo: function incrementa($valor1){} y por referencia se le agrega el (&) Ejemplo: function incrementa(&$valor1){}.
-
-function incrementa(&$valor1){
-
-    $valor1 +=2;
-
-        return $valor1;
-}
-
-$numero=5;
-
-    echo incrementa($numero) . "<br>";
-
-    echo $numero . "<br>" . "<br>";
+// POO creación e instanciación de Clases en PHP.
 
 
-function cambia_Mayus(&$param){
+ class Coche{
 
-    $param = strtolower($param);
 
-    $param= ucwords($param);
 
-    return $param;
 
-}
 
-$cadena = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum labore facilis neque, odit quia alias accusantium harum, accusamus corporis error autem odio minus. Ratione veniam velit possimus distinctio sapiente? Deleniti?";
+ }
 
-echo cambia_Mayus($cadena) . "<br>" . "<br>";
-
-echo $cadena;
+ $renault = new Coche();
+ $mazda = new Coche();
+ $volvo = new Coche();
 
 
 
