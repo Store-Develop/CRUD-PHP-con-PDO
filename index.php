4bec8ac829488d6fd5bf9969360a10b0c761174b
Juan Perez <jkml563@gmail.com>
2022-08-13 08:38:32 -0500

    POO II

--- index.php
+++ index.php
@@ -1,18 +1,48 @@
 <?php
-// POO creación e instanciación de Clases en PHP.
+// POO creación asignación de atributos de Clases en PHP.
 
 
  class Coche{
+        //Atributos de una clase u objeto.
+        var $ruedas;
+        var $color;
+        var $motor;
 
+        function Coche(){// Método constructor de la clase
 
+            $this->ruedas=4;
+            $this->color="";
+            $this->motor= 1600;
 
+        }
 
+    function arranca(){ // Métodos o funcionalidades de una clase u objeto.
+
+        echo "Estoy arrancando <br>";
+
+    }
+
+    function girar(){
+
+        echo "Estoy girando <br>";
+
+    }
+
+    function frenar(){
+
+        echo "Estoy frenando <br>";
+
+    }
 
  }
 
  $renault = new Coche();
  $mazda = new Coche();
  $volvo = new Coche();
+
+ $mazda->girar();
+ $mazda->frenar();
+ echo $mazda->ruedas;
 
 
 
