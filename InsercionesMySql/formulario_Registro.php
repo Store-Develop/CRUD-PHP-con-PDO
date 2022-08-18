0446ed6599163919f7ac2a5b8b76bb07f3383896
Juan Perez <jkml563@gmail.com>
2022-08-18 11:15:55 -0500

    Insertando registros en BBDD MySql

---
+++ InsercionesMySql/formulario_Registro.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/Style.css" type="text/css" />
+    <title>Formulario</title>
+   
+</head>
+<body>
+    <h1>Registro de Personal</h1>
+    <form name="form1" method="get" action="index.php">
+
+    <table border="0" align="center">
+        <tr>
+            <td class="Tlabel">Código:</td>
+            <td><label for="C_emp"></label>
+            <input type="number" name="C_emp" id="C_emp"></td>
+        </tr>
+
+        <tr>
+            <td class="Tlabel">Apellido:</td>
+            <td><label for="A_emp"></label>
+            <input type="text" name="A_emp" id="A_emp"></td>
+        </tr>
+
+        <tr>
+            <td class="Tlabel">Nombre:</td>
+            <td><label for="N_emp"></label>
+            <input type="text" name="N_emp" id="N_emp"></td>
+        </tr>
+
+        <tr>
+            <td class="Tlabel">Cargo:</td>
+            <td><label for="Cargo_emp"></label>
+            <input type="text" name="Cargo_emp" id="Cargo_emp"></td>
+        </tr>
+
+        <tr>
+            <td class="Tlabel">Departamento:</td>
+            <td><label for="D_emp"></label>
+            <input type="text" name="D_emp" id="D_emp"></td>
+        </tr>
+
+        <tr>
+            <td class="Tlabel">Salario:</td>
+            <td><label for="S_emp"></label>
+            <input type="number" name="S_emp" id="S_emp"></td>
+        </tr>
+
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+
+        <tr>
+            <td align="center"> <input type="submit" name="enviar" id="enviar" value="Enviar"/></td>
+            <td align="center"> <input type="reset" name="borrar" id="borrar" value="Borrar"/></td>
+        </tr>
+        
+    </table>
+    </form>
+</body>
+</html>
